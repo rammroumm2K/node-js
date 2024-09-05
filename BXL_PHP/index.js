a352e9e73c52b93cf3b95c8d0216ea4e8d50ada6
rammroumm2K <ramybrz00@gmail.com>
2024-09-05 16:25:14 +0200

    v1.0

---
+++ BXL_PHP/index.js
@@ -0,0 +1,46 @@
+// Importer le module express 
+const express = require('express');
+
+// créer un application 
+const app = express();
+
+
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// définir une route vers la page d'accueil (la racine du site)
+app.get('/', function(requete,response){
+    response.render('accueil.ejs', {title : "Accueil"});
+});
+app.get('/culture', function(requete,response){
+    response.render('culture.ejs', {title : "culture"});
+});
+app.get('/galerie', function(requete,response){
+    response.render('galerie.ejs', {title : "galerie"});
+});
+app.get('/geographie', function(requete,response){
+    response.render('geographie.ejs', {title : "geographie"});
+});
+app.get('/histoire', function(requete,response){
+    response.render('histoire.ejs', {title : "histoire"});
+});
+app.get('/liens', function(requete,response){
+    response.render('liens.ejs', {title : "liens"});
+});
+
+
+// définir une route vers la page contact
+app.get('/contact', function(requete,response){
+   response.render('contact.ejs' , {title : "contact"});
+});
+
+
+
+// définir une "page" gérant l'erreur 404 
+app.use(function(requete,response,next){
+    response.status(404).render('page-404.ejs',{title:"404"} );
+});
+
+app.listen(8080);  // le serveur web écoute sur le port 8080
+console.log("Express est démarré et attend votre requête...")
