dea9bf99d609e26b816e42b6eee07358f6d5f8c9
rammroumm2K <ramybrz00@gmail.com>
2024-09-10 15:49:53 +0200

    v 1.1.2

--- formulaire/contact/routes/index.js
+++ formulaire/contact/routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Affichage du form de contact (voir view contact_form.ejs). */
+router.get('/contact', function(req, res, next) {
+  res.render('contact_form.ejs');
+});
+
+//Définition du middleware qui va analyser le requête POST
+let urlEncodedParser= express.urlencoded({extended:false});
+
+router.post('/traitement', urlEncodedParser, function(req,res, next){
+  let lenom = req.body.nom;
+  let lemessage =req.body.msg;
+  res.render('traiter_form.ejs', {titre: 'Formulaire reçu', nom:lenom, msg:lemessage});
+});
+
 
 module.exports = router;
