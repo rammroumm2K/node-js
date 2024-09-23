05c4fc47cd96040913d6fb9ad065d850fb557c0f
rammroumm2K <ramybrz00@gmail.com>
2024-09-23 14:25:36 +0200

    v 1.5.b.a

--- crud/crudmv/routes/messages.js
+++ crud/crudmv/routes/messages.js
@@ -5,12 +5,30 @@ var router = express.Router();
 var messages = require("../controllers/message.controller.js");
 
 router.get('/',messages.readAll);
+//  ⭐⭐⭐⭐⭐⭐
 
 router.get('/list', messages.list);
-
+//  ⭐⭐⭐⭐⭐⭐
 
 router.get('/read/:id', messages.readById);
+//  ⭐⭐⭐⭐⭐⭐
 
+router.get('/newmsg', messages.newmsg);
+//  ⭐⭐⭐⭐⭐⭐
 
+router.post('/create', messages.create);
+//  ⭐⭐⭐⭐⭐⭐
+
+router.get('/edit/:id', messages.updateById);
+//  ⭐⭐⭐⭐⭐⭐
+
+router.post('/update/:id', messages.update);
+//  ⭐⭐⭐⭐⭐⭐
+
+router.get('/confirm/:id', messages.deleteById);
+//  ⭐⭐⭐⭐⭐⭐
+
+router.post('/delete/:id', messages.delte);
+//  ⭐⭐⭐⭐⭐⭐
 
 module.exports = router;
