d3e74ea15d04c2e2f039d054604faff84b3da1a8
rammroumm2K <ramybrz00@gmail.com>
2024-09-23 14:15:39 +0200

    v 1.5.b

--- crud/crudmv/routes/index.js
+++ crud/crudmv/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+//Controller root
+var root = require("../controllers/root.controller.js");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', root.home);
+
+// Afficher le formulaire 
+
+router.get('/contact',root.form);
+
+// aff les donnes entrees dans le formulaire
+
+router.post('/traitement', root.traitement);
+
 
 module.exports = router;
--- crud/crudmv/routes/messages.js
+++ crud/crudmv/routes/messages.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 
+var messages = require("../controllers/message.controller.js");
 
+router.get('/',messages.readAll);
+
+router.get('/list', messages.list);
+
+
+router.get('/read/:id', messages.readById);
 
 
 
